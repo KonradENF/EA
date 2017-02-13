59c3721992e85a7ab6cb49bc840d5aed9d43c9e8
Konrad <konrad.fahrendholz@itk-engineering.de>
2017-02-13 16:02:20 +0100

    Modify Build-Script
    Add Clear-Command

--- make.ps1
+++ make.ps1
@@ -183,7 +183,18 @@ function Check-SystemPath{
 
 }
 
+function Clear-Workspace{
+$exclude = $extDir + "*"
+
+Get-ChildItem -Path  $absoluteDir -Recurse |
+Select -ExpandProperty FullName |
+Where {$_ -notlike $exclude} |
+sort length -Descending |
+Remove-Item -force 
+}
+
 Check-SystemPath
+Clear-Workspace
 Install-Command
 
 BuildTest-Command
